bac6f377d210ade238019595f84af8d1f0c7e724
atique2107 <mdatiquepatna@gmail.com>
2024-07-10 05:06:46 +0530

    commented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import CategoryFilter from '@/components/shared/CategoryFilter';
-import Collection from '@/components/shared/Collection'
+//import Collection from '@/components/shared/Collection'
 import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
@@ -51,7 +51,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           <CategoryFilter />
         </div>
 
-        <Collection 
+        {/* <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -59,7 +59,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        />
+        /> */}
       </section>
     </>
   )
